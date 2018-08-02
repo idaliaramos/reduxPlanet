672f1cefa7e1deda6fdccd8fa620032b1ed881ce
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-08-02 15:46:26 -0700

    added fetch call in action

--- src/Planets/planetActions.jsx
+++ src/Planets/planetActions.jsx
@@ -1,8 +1,22 @@
 const getPlanet = () => {
-  return {
-    type: "GET_PLANET"
+  return dispatch => {
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=41VnYPWSO4p5gRvZPMgDzPls94Hnm0pzkAq8q5fu`
+    )
+      .then(response => response.json())
+      .then(planet =>
+        dispatch({
+          type: "GET_PLANET",
+          payload: planet
+        })
+      );
+    //   .catch(err => dispatch({
+    //     type: FETCH_TODOS_FAILED,
+    //     payload: err
+    //   })
+    // )
   };
-};
+}
 //many other functions
 
 export { getPlanet };
